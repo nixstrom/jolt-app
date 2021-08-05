cf774f767deaf19ea0c29e07867c92e8a53785f6
Nikki Strømsnes <nikkistromsnes@gmail.com>
2021-08-05 19:31:35 +0200

    Update fixture to make data not expire immediately

--- fixtures/notes.ts
+++ fixtures/notes.ts
@@ -1,22 +1,22 @@
-import { Note } from "@types";
+import { Note } from "types";
 
 export const notes: Note[] = [
   {
     id: "1",
     body: "Hello, World!",
-    createdAt: 1627841300136,
+    createdAt: 1727841300136,
     pinned: false,
   },
   {
     id: "2",
     body: "Here is a cool not2e",
-    createdAt: 1627841300136,
+    createdAt: 1727841300136,
     pinned: false,
   },
   {
     id: "3",
     body: "Buy milk",
-    createdAt: 1627841300136,
+    createdAt: 1727841300136,
     pinned: false,
   },
 ];
