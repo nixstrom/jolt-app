feee4b32cb6ea9b4a85aaa270a61ae14553b105a
Nikki Strømsnes <nikkistromsnes@gmail.com>
2021-08-05 19:31:57 +0200

    Add test setup

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  rootDir: ".",
+  verbose: true,
+  preset: "react-native",
+  testPathIgnorePatterns: ["<rootDir>/node_modules"],
+  moduleFileExtensions: ["js", "ts", "tsx"],
+};
+export default config;
